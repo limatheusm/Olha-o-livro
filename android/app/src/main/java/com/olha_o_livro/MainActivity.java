36711fe7d42bf1e2dedd0ae691bb86e08700802f
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-10-26 19:09:05 -0300

    First commit

---
+++ android/app/src/main/java/com/olha_o_livro/MainActivity.java
@@ -0,0 +1,15 @@
+package com.olha_o_livro;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Olha_o_livro";
+    }
+}
