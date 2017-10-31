bd87cd23b5643033a29dee0ef422ffcb4bb7656b
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-10-30 23:54:03 -0200

    Corrige TabBar no Android para Bottom

--- src/RootTabs.js
+++ src/RootTabs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, TabBarBottom  } from 'react-navigation';
 import Entypo from 'react-native-vector-icons/Entypo'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -8,46 +8,55 @@ import MaterialsScreen from './components/pages/MaterialsScreen'
 import MyAccountScreen from './components/pages/MyAccountScreen'
 import AnnounceScreen from './components/pages/AnnounceScreen'
 
-const RootTabs = TabNavigator({
-    Material: {
-        screen: MaterialsScreen,
-        navigationOptions: {
-          tabBarLabel: 'Materiais',
-          tabBarIcon: ({ tintColor, focused }) => (
-            <Entypo
-              name={focused ? 'open-book' : 'book'}
-              size={26}
-              style={{ color: tintColor }}
-            />
-          ),
-        },
-    },
-    Announce: {
-        screen: AnnounceScreen,
-        navigationOptions: {
-            tabBarLabel: 'Anunciar',
+const RootTabs = TabNavigator(
+    {
+        Material: {
+            screen: MaterialsScreen,
+            navigationOptions: {
+            tabBarLabel: 'Materiais',
             tabBarIcon: ({ tintColor, focused }) => (
-                <Ionicons 
-                    name={focused ? 'ios-megaphone' : 'ios-megaphone-outline'}
+                <Entypo
+                name={focused ? 'open-book' : 'book'}
+                size={26}
+                style={{ color: tintColor }}
+                />
+            ),
+            },
+        },
+        Announce: {
+            screen: AnnounceScreen,
+            navigationOptions: {
+                tabBarLabel: 'Anunciar',
+                tabBarIcon: ({ tintColor, focused }) => (
+                    <Ionicons 
+                        name={focused ? 'ios-megaphone' : 'ios-megaphone-outline'}
+                        size={26}
+                        style={{ color: tintColor }}
+                    />
+                )
+            }
+        },
+        MyAccount: {
+            screen: MyAccountScreen,
+            navigationOptions: {
+                tabBarLabel: 'Minha conta',
+                tabBarIcon: ({ tintColor, focused }) => (
+                <MaterialCommunityIcons
+                    name={focused ? 'account' : 'account-outline'}
                     size={26}
                     style={{ color: tintColor }}
                 />
-            )
+                ),
+            },
         }
     },
-    MyAccount: {
-        screen: MyAccountScreen,
-        navigationOptions: {
-            tabBarLabel: 'Minha conta',
-            tabBarIcon: ({ tintColor, focused }) => (
-              <MaterialCommunityIcons
-                name={focused ? 'account' : 'account-outline'}
-                size={26}
-                style={{ color: tintColor }}
-              />
-            ),
+    {
+        tabBarOptions: {
+            activeTintColor: 'dodgerblue',
         },
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: 'bottom',
     }
-});
+);
 
 export default RootTabs;
