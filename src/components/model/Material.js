d4dba221cc4234d1fa15fa1b45b23d2a602290f4
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-11-09 21:35:05 -0300

    Cria Donator e atualiza Material

--- src/components/model/Material.js
+++ src/components/model/Material.js
@@ -1,13 +1,16 @@
-
+import { Donator } from './Donator'
 class Material {
     constructor(key) {
-        this.title = "Livro de Python " + key
-        this.type = "Emprestimo"
-        this.description = "Lorem ipsum"
-        this.imageURL = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224083.jpg"
-        this.local = "UFPB"
-        this.date = "10/10/2017"
-        this.key = key
+        this.title = "Livro de Python " + key;
+        this.type = "Empréstimo";
+        this.description = "Este livro é orientado ao iniciante em programação. Os conceitos básicos de programação, " +
+                           "como expressões, variáveis, repetições, decisões, listas, funções, arquivos e banco de dados.";
+        this.imageURL = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224083.jpg";
+        this.local = "UFPB";
+        this.date = "10/10/2017";
+        this.donator = new Donator();
+        this.heart = 54;
+        this.key = key;
     }
 }
 
--- src/components/model/index.js
+++ src/components/model/index.js
@@ -1,1 +1,2 @@
-export * from './Material'
+export * from './Material';
+export * from './Donator';
