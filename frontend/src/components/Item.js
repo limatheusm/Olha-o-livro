38d08c809e2ec0d621b1b6e31a7d358d516cb5bf
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-11-26 17:33:26 -0300

    Refatora Item

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -18,10 +18,10 @@ export default class Item extends Component {
 
     _onPress() {
         console.log(this.state.material.title)
-        if (this.props.editMaterial === 'edit') {
+        if (this.props.editMaterial) {
             this.props.navigation.navigate('EditMaterial', { material: this.state.material })
-        }else{
-        this.props.navigation.navigate('Details', { material: this.state.material })
+        } else {
+            this.props.navigation.navigate('Details', { material: this.state.material })
         }
     }
 
