e77a678c73ffb8b483531a4f42a6a52ca0a90ee0
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-11-28 18:39:02 -0300

    Atualiza Atributos de acordo com o schema do banco

--- frontend/src/business/model/user/User.js
+++ frontend/src/business/model/user/User.js
@@ -1,7 +1,9 @@
 class User {
     constructor() {
-        this.name = 'User 1';
+        this._id = '';
+        this.name = 'User';
         this.mail = 'user@olhaolivro.com';
+        this.password = 'Lima1234567';
         this.phone = '(83) 9 9988-8899';
         this.imageURL = 'https://scontent.frec3-2.fna.fbcdn.net/v/t1.0-9/20292831_1563732526991471_5299144803312892119_n.jpg?oh=82be979c7aebcc7dac4a9d6599e4e7a5&oe=5AA57AA0';
         this.materials = [];
